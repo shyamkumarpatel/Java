53979d20e905e943ae3b4159b1e823336433f331
Shyam Patel <shyam.psu.lv@gmail.com>
2022-08-09 07:20:02 -0400

    Update APIController.java

--- daikichi-path-variables/src/main/java/com/example/demo/controllers/APIController.java
+++ daikichi-path-variables/src/main/java/com/example/demo/controllers/APIController.java
@@ -2,19 +2,33 @@ package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class APIController {
-	
+    @RequestMapping("/daikichi")
+    public String index() {
+        return "Welcome!";
+    }	
+
+    @RequestMapping("/daikichi/today")
+    public String index2() {
+        return "Today you will find luck in all your endeavors!'";
+    }	
+
+    @RequestMapping("/daikichi/tomorrow")
+    public String index3() {
+        return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }	
+    
+    
     @RequestMapping("/daikichi/travel/{location}")
-    public String index(@PathVariable("location") String location) {
+    public String index4(@PathVariable("location") String location) {
         return "Congratulations! You will soon travel to " + location + "!";
     }	
     
     @RequestMapping("/daikichi/lotto/{number}")
-    public String index(@PathVariable("number") Integer number) {
+    public String index5(@PathVariable("number") Integer number) {
     	if(number%2 == 0) {
     		return "You will take a grand journey in the near future, but be weary of tempting offers";
     	}
